0174f486ba88b173b40b1773780ed858aceeaf14
Avinash-Alapati <avinashalapati11@gmail.com>
2026-07-12 12:54:46 +0530

    Implemented Mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,27 @@
-import dotenv from "dotenv";
+import { env } from "./config/env";
+import { connectDatabase } from "./config/database";
+import mongoose from "mongoose";
 import app from "./app";
 
-dotenv.config();
+const startServer = async () => {
+  await connectDatabase();
 
-const PORT = process.env.PORT || 5000;
+  const server = app.listen(env.PORT, () => {
+    console.log(
+      `🚀 Server running at http://localhost:${env.PORT}`
+    );
+  });
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+  process.on("SIGINT", async () => {
+    console.log("\n🛑 Shutting down server...");
+
+    await mongoose.connection.close();
+
+    server.close(() => {
+      console.log("✅ Server stopped");
+      process.exit(0);
+    });
+  });
+};
+
+startServer();
